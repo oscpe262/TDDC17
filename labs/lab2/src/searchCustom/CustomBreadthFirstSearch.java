785f73f1a9d51c837b9f6312b6b5c3d6e8876f05
oscpe262 <oscpe262@student.liu.se>
2016-09-12 08:36:02 +0200

    Fixfixfix

--- labs/lab2/src/searchCustom/CustomBreadthFirstSearch.java
+++ labs/lab2/src/searchCustom/CustomBreadthFirstSearch.java
@@ -1,15 +1,8 @@
 package searchCustom;
-
 import java.util.Random;
-
 public class CustomBreadthFirstSearch  extends CustomGraphSearch{
-
 	public   CustomBreadthFirstSearch(int maxDepth){
-<<<<<<< HEAD
 	    super(false); 
-=======
-		super(false); // Temporary random choice, you need to pick true or false!
-		System.out.println("Change line above in \"CustomBreadthFirstSearch.java\"!");
->>>>>>> baa13b98b3a8fe1f730870ef667c28c04c14ff48
+	    //		System.out.println("Change line above in \"CustomBreadthFirstSearch.java\"!");
 	}
 };
